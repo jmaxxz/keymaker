c408d4bab2b50fbfcd6a896d6a1e898a6940fddf
jmaxxz <jmaxxz@users.noreply.github.com>
2016-07-27 23:14:14 -0500

    Add ability to extract firmwarekey from lock

--- node_tools/src/get_keys_from_lock.js
+++ node_tools/src/get_keys_from_lock.js
@@ -10,22 +10,24 @@ const Environment = require('./lib/environment')
 require('./lib/async_logging');
 
 const keyMap = [
+  { start: 32196, count: 1, offset: 4, initialSlot: 0, ignoreChecksum: true },
   { start: 17410, count: 99, offset: 4, initialSlot: 1 },
   { start: 17920, count: 100, offset: 0, initialSlot: 100 },
-  { start: 18432, count: 56, offset: 0, initialSlot: 200 }
+  { start: 18432, count: 57, offset: 0, initialSlot: 200 }
   ];
 
 const environment = new Environment(environmentConfig);
 
-function extractKeys(buffer, offset, initialSlot) {
+function extractKeys(buffer, offset, initialSlot, count, ignoreChecksum) {
   var results = [];
-  for (let i = offset; i < buffer.length-20; i +=20) {
-    let expectedChecksum = (-(buffer.readInt32LE(i) + buffer.readInt32LE(i + 4)
-      + buffer.readInt32LE(i + 8) + buffer.readInt32LE(i + 12))) & 0xFFFFFFFF;
-    let readChecksum = buffer.readInt32LE(i + 16);
+  for (let i = 0; i < count; i++) {
+    var j = offset + i * 20;
+    let expectedChecksum = (-(buffer.readInt32LE(j) + buffer.readInt32LE(j + 4)
+      + buffer.readInt32LE(j + 8) + buffer.readInt32LE(j + 12))) & 0xFFFFFFFF;
+    let readChecksum = buffer.readInt32LE(j + 16);
 
-    if(expectedChecksum == readChecksum) {
-      results.push({keySlot:(initialSlot + (i - offset)/20), key:buffer.toString('hex', i, i + 16)});
+    if(expectedChecksum == readChecksum || ignoreChecksum) {
+      results.push({keySlot:(initialSlot + i), key: buffer.toString('hex', j, j + 16)});
     }
   }
   return results;
@@ -44,17 +46,18 @@ async function onSecResponse(session, state, data) {
     state.buffer.write(secCmd.parameter1, (i - keyMap[state.part].start) * 8, 'hex');
     state.buffer.write(secCmd.parameter2, (i - keyMap[state.part].start) * 8 + 4, 'hex');
 
-    if((i-keyMap[state.part].start)*8 < 20 * (keyMap[state.part].count-1) + keyMap[state.part].offset/8) {
+    if((i-keyMap[state.part].start)*8 < 20 * (keyMap[state.part].count) + keyMap[state.part].offset/8) {
       return await session.secWrite(cmd.readSecFlash(i + 1).data);
     }
 
     //The current part of the keymap has been extracted
-    var newResults = extractKeys(state.buffer, keyMap[state.part].offset, keyMap[state.part].initialSlot);
+    var newResults = extractKeys(state.buffer, keyMap[state.part].offset,
+      keyMap[state.part].initialSlot, keyMap[state.part].count, keyMap[state.part].ignoreChecksum);
     state.results = state.results.concat(newResults);
     printKeys(newResults);
 
     if(state.part++ < keyMap.length - 1) {
-      state.buffer = Buffer.alloc(20 * keyMap[state.part].count + keyMap[state.part].offset);
+      state.buffer = Buffer.alloc(20 * keyMap[state.part].count + keyMap[state.part].offset + 20);
       return await session.secWrite(cmd.readSecFlash(keyMap[state.part].start).data);
     }
 
@@ -67,7 +70,7 @@ async function onSecResponse(session, state, data) {
 
 async function onSessionStart(session) {
   var mapPartIndex = 0;
-  var buffer = Buffer.alloc(20 * keyMap[mapPartIndex].count + keyMap[mapPartIndex].offset);
+  var buffer = Buffer.alloc(20 * keyMap[mapPartIndex].count + keyMap[mapPartIndex].offset + 20);
   var state = { results: [], part: mapPartIndex, buffer: buffer };
   session.on('secUpdate', d=>onSecResponse(session, state, d));
   await session.secWrite(cmd.readSecFlash(keyMap[mapPartIndex].start).data);
